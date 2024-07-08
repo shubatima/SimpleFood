ac7ba3e439c1d294f917276ac155ff23013d1c33
Tymofey Shubovych <subovichtimofey23@gmail.com>
2024-07-08 21:44:52 +0300

    not full homework about all section on laptop view

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,17 @@
 $(function () {
 
-   
-  
+  $('.feedback__slider').slick({
+
+    arrows: true,
+    dots: true,
+
+    prevArrow: '<button type="button" class="feedback__btn feedback__btn--prev"><svg class="feedback__arrow feedback__arrow--left"><use xlink:href="images/sprite.svg#icon-vector"></use></svg></button>',
+
+    nextArrow: '<button type="button" class="feedback__btn feedback__btn--next"><svg class="feedback__arrow feedback__arrow--right"><use xlink:href="images/sprite.svg#icon-vector"></use></svg></button>',
+
+    appendArrows: '.arrows-wrap'
+  })
+
 })
 
-var mixer = mixitup('.product'); 
+var mixer = mixitup('.product');
--- gulpfile.js
+++ gulpfile.js
@@ -36,6 +36,7 @@ function styles() {
 function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
+    'node_modules/slick-carousel/slick/slick.min.js',
     'node_modules/mixitup/demos/mixitup.min.js',
     'app/js/main.js'
   ])
